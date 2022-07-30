87f9a3a1334144c9a6ddd73c56dd7256bd1c7a01
abhinavburra <64180259+abhinavburra@users.noreply.github.com>
2022-07-30 18:12:26 +0530

    hi

---
+++ com.abhi/src/main/java/com/ust/com/abhi/App.java
@@ -0,0 +1,13 @@
+package com.ust.com.abhi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Devops World!" );
+    }
+}
